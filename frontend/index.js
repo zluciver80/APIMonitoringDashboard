955b633242402e72852584f3a250c494d2ab52a2
zluciver80 <64784227+zluciver80@users.noreply.github.com>
2025-04-10 17:25:56 +0300

    improve variables

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const appRootContainer = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
+appRootContainer.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
